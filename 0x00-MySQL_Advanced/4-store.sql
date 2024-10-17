bebbf8ccec60863b2349de891fbe5f56b4891a04
ismael <spacingismael@gmail.com>
2024-10-17 13:00:17 +0100

    fixing task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,6 @@
 -- SQL script that creates a trigger that decreases the quantity of an item
-AFTER
-    adding a new order.
 CREATE trigger decrement
-AFTER
-INSERT
+AFTER INSERT
     ON orders FOR each ROW
 UPDATE
     items

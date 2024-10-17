f3d7154aefd7cc1a9fdcd72a5cf5da8886ad882c
ismael <spacingismael@gmail.com>
2024-10-17 12:12:13 +0100

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,4 @@
-SELECT
-    band_name,
-    (IFNULL(split, 2020) - formed) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style LIKE "%Glam rock%"
-ORDER BY
-    lifespan DESC
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE "%Glam rock%"
+ORDER BY lifespan DESC

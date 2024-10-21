06c79aca3e1ec3f4015a3a6c8fe713471adeed04
ismael <spacingismael@gmail.com>
2024-10-21 16:51:04 +0100

    fixing task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Insert a document in Python """
 
-if __name__ == '__main__':
-    def insert_school(mongo_collection, **kwargs):
-        """ function that inserts a new document
-        in a collection based on kwargs """
-        x = mongo_collection.insert_one(kwargs)
-        return x.inserted_id
+
+def insert_school(mongo_collection, **kwargs):
+    """ function that inserts a new document
+    in a collection based on kwargs """
+    x = mongo_collection.insert_one(kwargs)
+    return x.inserted_id

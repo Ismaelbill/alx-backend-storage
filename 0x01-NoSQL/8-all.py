770176421e466ae40a72e025be44da320d1836f5
ismael <spacingismael@gmail.com>
2024-10-21 16:49:42 +0100

    fixing task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Listing all documents in Python """
 
-if __name__ == '__main__':
-    def list_all(mongo_collection):
-        """ unction that lists all documents in a collection """
-        return mongo_collection.find()
+
+def list_all(mongo_collection):
+    """ unction that lists all documents in a collection """
+    return mongo_collection.find()

4769e6fb857d4b7e0f12284a247bcac0b121171d
ismael <spacingismael@gmail.com>
2024-10-24 14:10:28 +0100

    fixing checker

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writing strings to Redis """
 
 
-import redis as rds
+import redis
 import typing as t
 from uuid import uuid4
 
@@ -12,11 +12,11 @@ class Cache:
     """
     def __init__(self) -> None:
         """ creating instance of redis client"""
-        self._redis: rds.Redis = rds.Redis()
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: t.Union[str, bytes, int, float]) -> str:
         """  method that takes a data argument and returns a string. """
-        key: str = str(uuid4())
+        key = str(uuid4())
         self._redis.set(key, data)
         return key

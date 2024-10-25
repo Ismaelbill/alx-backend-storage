e891d65dd7219483ec9ed7fd6fccf1bbc67c946f
ismael <spacingismael@gmail.com>
2024-10-25 15:12:40 +0100

    task 1 in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,3 +20,23 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: t.Optional[callable] = None) -> any:
+        val = self._redis.get(key)
+        if not val:
+            return
+        if fn is int:
+            return self.get_int(val)
+        if fn is str:
+            return self.get_str(val)
+        if callable(fn):
+            return fn(val)
+        return val
+
+    def get_str(self, arg):
+        """ converts arg to str """
+        return arg.decode('utf-8')
+
+    def get_int(self, arg):
+        """ converts arg to int """
+        return int(arg)
